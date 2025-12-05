5ea590cbb9642919ae687ec2d56137e06d516065
Light-Fighter-Manifesto <git@lightfightermanifesto.org>
2025-12-05 08:53:09 -0500

    Bump service worker cache version to v2

--- static/apps/field-guide/sw.js
+++ static/apps/field-guide/sw.js
@@ -3,7 +3,7 @@
  * Enables offline functionality by caching app shell and data
  */
 
-const CACHE_NAME = 'lfm-field-guide-v1';
+const CACHE_NAME = 'lfm-field-guide-v2';
 
 // Files to cache on install
 const PRECACHE_URLS = [
